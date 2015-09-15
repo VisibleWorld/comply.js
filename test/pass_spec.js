49f886f9ebc3f754ae8d3cf561dcdc021ee7454d
Peter Zalewski <peterz@visibleworld.com>
2015-09-15 16:11:09 -0400

    Refactor and forbid pass() from allowing no value
    Validation is now decoupled from the schema in anticipation of
    allowing types and modules to test individual values with custom
    outside of the scope of a schema. This required changing logic
    on required fields, which fixed a longstanding bug/feature in
    the pass() type.

--- test/pass_spec.js
+++ test/pass_spec.js
@@ -10,27 +10,39 @@ var expect = require('chai').expect;
 describe('Schema', function() {
     describe('.test()', function() {
         describe('Pass()', function() {
+            var schema = new Schema({ foo: passType() });
 
-            it('should validate undefined', function() {
-                var schema = new Schema({ foo: passType() });
-
+            it('should not validate undefined', function() {
                 var result = schema.test({ foo: undefined });
 
-                expect(result.valid).to.be.true;
-                expect(result.object.foo).to.equal(undefined);
+                expect(result.valid).to.be.false;
             });
 
-            it('should validate null', function() {
-                var schema = new Schema({ foo: passType() });
-
+            it('should not validate null', function() {
                 var result = schema.test({ foo: null });
 
+                expect(result.valid).to.be.false;
+            });
+
+            it('should validate false', function() {
+                var result = schema.test({ foo: false });
+
                 expect(result.valid).to.be.true;
-                expect(result.object.foo).to.be.null;
+            });
+
+            it('should validate 0', function() {
+                var result = schema.test({ foo: 0 });
+
+                expect(result.valid).to.be.true;
+            });
+
+            it('should validate empty array', function() {
+                var result = schema.test({ foo: []});
+
+                expect(result.valid).to.be.true;
             });
 
             it('should validate some value', function() {
-                var schema = new Schema({ foo: passType() });
                 var o = { foo: 3 };
 
                 var result = schema.test(o);
@@ -40,7 +52,6 @@ describe('Schema', function() {
             });
 
             it('should validate some object', function() {
-                var schema = new Schema({ foo: passType() });
                 var o = { foo: { bar: 6 } };
 
                 var result = schema.test(o);
